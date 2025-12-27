21d7bf2d9113422e64bf83bd430ede7caeea49f7
ketan  funde <kfunde@capritechnosys.com>
2025-12-27 14:52:43 +0530

    ui bootstrap template integration

---
+++ MediCareWeb/Services/Implementations/Auth.cs
@@ -0,0 +1,32 @@
+﻿using MediCare.Dto.Auth;
+using MediCareDto.Auth;
+using MediCareWeb.Services.Interfaces;
+
+namespace MediCareWeb.Services.Implementations
+{
+    public class Auth : IAuth
+    {
+        private readonly HttpClient _httpClient;
+        private readonly IConfiguration _configuration;
+        private readonly string _backendUrl;
+
+
+        public Auth(HttpClient httpClient, IConfiguration configuration)
+        {
+            _httpClient = httpClient;
+            _configuration = configuration;
+            _backendUrl = _configuration["BackendUrl"] ?? throw new InvalidOperationException("BackendUrl not found in configuration.");
+
+        }
+
+        public async Task<AuthResponse> Login(LoginRequest model)
+        {
+            var result = await _httpClient.PostAsJsonAsync($"{_backendUrl}/api/auth/login", model);
+            if (result != null) {
+
+                return null;
+            }
+            return await result.Content.ReadFromJsonAsync<AuthResponse>();
+        }
+    }
+}
---
+++ MediCareWeb/Services/Interfaces/IAuth.cs
@@ -0,0 +1,10 @@
+﻿using MediCare.Dto.Auth;
+using MediCareDto.Auth;
+
+namespace MediCareWeb.Services.Interfaces
+{
+    public interface IAuth
+    {
+        Task<AuthResponse> Login(LoginRequest model);
+    }
+}
